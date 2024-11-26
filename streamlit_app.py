c2da6830acb33c549cc6dba4f698687aa6ff07d5
livebetedge <sarah.passafiume@gmail.com>
2024-11-26 16:59:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,3 @@ st.title ("Buffalo Sabres Shot by Period Data")
 st.write ("Please use the dropdown to filter players and see recent shot data")
 st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Tage Thompson', 'Jason Zucker','Alex Tuch','Dylan Cozens'])
 #function to load Shot data
-@st.cache_data
-def load_2024 data ():
-base_url = "http://raw.githubusercontent.com/livebetedge/blank-app-1/main"
-combined_data = pd.DataFrame
