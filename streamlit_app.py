f59fdd9d97df52375d64029f72950f5797c3a125
livebetedge <sarah.passafiume@gmail.com>
2024-11-26 16:42:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
-import streamlit as st
 import pandas as pd 
 import numpy as npy 
+import streamlit as st
+import altair as alt
+import requests
+from io import BytesIO
 st.title ("Buffalo Sabres Shot by Period Data")
 st.write ("Please use the dropdown to filter players and see recent shot data")
 st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Tage Thompson', 'Jason Zucker','Alex Tuch','Dylan Cozens'])
+#function to load Shot data
+@st.cache_data
+def load_2024 data ():
