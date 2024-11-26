a1a63e0e4b72150e304d6c69f9612a1642f87458
livebetedge <sarah.passafiume@gmail.com>
2024-11-26 17:22:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,7 @@ import streamlit as st
 import altair as alt
 import requests
 from io import BytesIO
-st.title ("Buffalo Sabres Shot by Period Data")
-st.write ("Please use the dropdown to filter players and see recent shot data")
-st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Tage Thompson', 'Jason Zucker','Alex Tuch','Dylan Cozens'])
 #function to load Shot data
-# Mapping pitch type abbreviations to full text
 
 # Function to load 2024 shot data from GitHub 
 @st.cache_data
