366e7306243622af24fa4426d051ca7fb1f3c671
livebetedge <sarah.passafiume@gmail.com>
2024-11-26 18:57:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,4 @@ import altair as alt
 import requests
 from io import BytesIO
 from pybaseball import statcast
-statcast(start_dt="2024-10-01", end_dt="2024-10-31").columns
-
+statcast(start_dt="2024-10-01", end_dt="2024-10-31")
