cb1be25a3c8e40e5c751c8f2e393de674939fa8a
livebetedge <sarah.passafiume@gmail.com>
2024-11-26 11:23:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import pandas as pd
 import numpy as npy 
 st.title ("Buffalo Sabres Shot by Period Data")
 st.write ("Please use the dropdown to filter players and see recent shot data")
-st.selectbox (label = "Player Name", options = "Rasmus Dahlin", "Tage Thompson", "Jason Zucker")
+st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Tage Thompson', 'Jason Zucker'])
