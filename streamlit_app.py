71fab50bede0dcbf7298a86ccb910715f7c71e65
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 06:45:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,11 @@ import requests
 from io import BytesIO
 st.title ('Buffalo Sabres Shot by Period Data')
 st.write ('Please use the drop down menu below to select a player and see his recent shot data')
-st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Dylan Cozens'])
-if 'Rasmus Dahlin' in 'Player Name':
-    Rasmus_Dahlin_id = st.text_input('Rasmus Dahlin is a 1st period shot attempt leader')
+option = st.multiselect(
+    'Player Name',
+    ['Rasumus Dahlin', 'Dylan Cozens', 'JJ Peterka'])
 
+if 'Rasmus Dahlin' in option:
+    Rasmus_Dahlin_id = st.text_input('Rasmus Dahlin is a 1st period shot leader')
+   
 
