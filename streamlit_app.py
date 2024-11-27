dbadd97037702b1618f91f359d39a36e1c9f0783
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 07:19:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,12 @@ from io import BytesIO
 st.title ('Buffalo Sabres Shot by Period Data')
 st.write ('Please use the drop down menu below to select a player and see his recent shot data')
 st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Dylan Cozens'])
-df = pd.read_csv('Sabres Shot Data - 11.20')
-print (df)
+
+# Updated URL to the raw CSV content
+raw_url = 'https://raw.githubusercontent.com/livebetedge/blank-app-1/main/Sabres%20Shot%20Data%20-%2011.20.csv'  
+
+df = pd.read_csv(raw_url)
+print(df)
 
    
 
