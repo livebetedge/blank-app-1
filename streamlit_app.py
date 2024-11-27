4494dd19a179006819d69fd3bcb4917fff1a88d2
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 07:06:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,8 @@ import requests
 from io import BytesIO
 st.title ('Buffalo Sabres Shot by Period Data')
 st.write ('Please use the drop down menu below to select a player and see his recent shot data')
-option = st.multiselect(
-    'Player Name',
-    ['Rasmus Dahlin', 'Dylan Cozens', 'JJ Peterka'])
-df = pd.read_csv('Sabres Shot Data -11.20')
+st.selectbox ('label' = 'Player Name', options = ['Rasmus Dahlin', 'Dylan Cozens', 'JJ Peterka'])
+df = pd.read_csv('Sabres Shot Data - 11.20')
 print (df)
 
    
