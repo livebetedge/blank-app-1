a0c332fd361b5dacb84ff790a59f5217cb1c4847
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 06:34:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ import requests
 from io import BytesIO
 st.title ('Buffalo Sabres Shot by Period Data')
 st.write ('Please use the drop down menu below to select a player and see his recent shot data')
-st.selectbox (label = 'Player Name', option = ['Rasmus Dahlin', 'Dylan Cozens'])
+st.selectbox (label = 'Player Name', options = ['Rasmus Dahlin', 'Dylan Cozens'])
