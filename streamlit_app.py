713d4d37f14a3c6d08627a4fea871d5055a5c22f
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 06:32:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import streamlit as st
 import altair as alt
 import requests
 from io import BytesIO
-url = 'https://github.com/livebetedge/blank-app-1/blob/main/Shot%20Data%20By%20Period.xlsx%20-%20Sabres%20Shots.csv'
-df = pd.read_csv(url,index_col=0)
-#df = pd.read_csv(url)
-
-print(df.head(5))
+st.title ('Buffalo Sabres Shot by Period Data')
+st.write ('Please use the drop down menu below to select a player and see his recent shot data')
+st.selectbox (label = 'Player Name', option = ['Rasmus Dahlin', 'Dylan Cozens'])
