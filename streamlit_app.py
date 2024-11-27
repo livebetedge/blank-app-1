942d3676591b6a2e1236f440f76078e353ce13b4
livebetedge <sarah.passafiume@gmail.com>
2024-11-27 07:40:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import altair as alt
 import requests
 from io import BytesIO
 df = pd.read_csv("https://github.com/livebetedge/blank-app-1/blob/main/Sabres%20Shot%20Data%20-%2011.20.csv")  
-st.title("Buffalo Sabres Shot Data")  # add a title
+st.title("Buffalo Sabres Shot Data")
 st.write(df) 
 
 
